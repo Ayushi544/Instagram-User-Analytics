d363fe14451bd6a31b92ffe8c171486aedaee432
Ayushi Dadhich <71173604+Ayushi544@users.noreply.github.com>
2023-05-09 21:49:22 +0530

    Update Code.sql

--- Code.sql
+++ Code.sql
@@ -10,20 +10,26 @@ on users.id = photos.user_id
 where photos.id is null;
 
 #3. Declaring Contest Winner: Identify the winner of the contest and provide their details to the team
-inner join likes on likes.photo_id = photos.id
-inner join users on photos.user_id = users.id
-group by photos.id 
-order by total desc
-limit 1;
+SELECT username,photos.id,photos.image_url,
+	   count(*) as total
+FROM photos
+INNER JOIN likes 
+    ON likes.photo_id = photos.id
+INNER JOIN users 
+    ON photos.user_id = users.id
+GROUP BY photos.id 
+ORDER BY total desc
+LIMIT 1;
 
 #4. Hashtag Researching: Identify and suggest the top 5 most commonly used hashtags on the platform
-select tags.tag_name,
-	    count(*) as total
-from photo_tags
-     join tags on photos_tags.tag_id = tags.id
-group by tags.id
-order by total desc
-limit 5
+SELECT tags.tag_name,
+       count(*) as total
+FROM photo_tags
+     JOIN tags 
+       ON photos_tags.tag_id = tags.id
+GROUP BY tags.id
+ORDER BY total desc
+LIMIT 5;
 
 #5. Launch AD Campaign: What day of the week do most users register on? Provide insights on when to schedule an ad campaign
 Select  
@@ -35,12 +41,13 @@ order by total desc
 limit 7;
 
 #6. User Engagement: Provide how many times does average user posts on Instagram. Also, provide the total number of photos on Instagram/total number of users
-select(select count(*) from photos)/(select count(*) from users as avg;
+select(select count(*)
+       from photos)/(select count(*) 
+                     from users) as avg;
     
 #7. Bots & Fake Accounts: Provide data on users (bots) who have liked every single photo on the site (since any normal user would not be able to do this).
-select username, 
-       count(*) as num_likes
+select username, count(*) as num_likes
 from users
-       Inner join likes on users.id = likes.user_id
+Inner join likes on users.id = likes.user_id
 group by likes.user_id
 having num_likes = (select count (*) from photos);
